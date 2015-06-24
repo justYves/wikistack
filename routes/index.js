0a0eccbd0cf038b177c423c190932894eaaa116a
Kevin Lei <knl35@cornell.edu>
2015-06-24 12:46:34 -0400

    Completed initial home page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WikiStack' });
+	models.Page.find(function(err, data){
+		res.render('index', { docs: data });
+	});
 });
+
+
+router.get('/wiki/:title', function(req, res, next){
+	var title = req.params.title;
+
+	models.Page.findOne({ url_name: title }, function(err, data){
+		res.render('show', {doc: data});
+	});
+
+
+})
+
 
 module.exports = router;
