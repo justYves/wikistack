1b058d1748f8bfd61d422d166acb83b1fcbbc1af
Yves Yuen <yves.j.yuen@gmail.com>
2015-06-24 13:55:43 -0400

    Added Marked library

--- filter.js
+++ filter.js
@@ -1,4 +1,7 @@
+
+
 module.exports = function(swig) {
+
   var page_link = function (doc) {
     var link_name;
     if (typeof doc.title !== "undefined" && doc.title !== "") {
@@ -9,6 +12,12 @@ module.exports = function(swig) {
     return "<a href='"+doc.full_route+"'>"+link_name+"</a>";
   };
   page_link.safe = true;
-
   swig.setFilter('page_link', page_link);
+
+  var marked = require('marked');
+  var markedFilter = function(doc){
+    return marked(doc);
+  }; 
+  markedFilter.safe = true;
+  swig.setFilter('marked',markedFilter);
 };
